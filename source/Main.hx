7f12d159ea227802ba86bea5a881e113c944b1ef
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-07-30 16:31:19 +0300

    SKURODBQLFHQLFBSLFBF

--- source/Main.hx
+++ source/Main.hx
@@ -12,7 +12,7 @@ import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
 import states.TitleState;
-import backend.SUtil;
+import SUtil;
 
 //crash handler stuff
 #if CRASH_HANDLER
@@ -84,10 +84,16 @@ class Main extends Sprite
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
 	    SUtil.doTheCheck();
+
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
+	
+		#if mobile
+		addChild(new FlxGame(1280, 720, TitleState, 60, 60, true, false));
+		#else
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		#end
 
 	
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
