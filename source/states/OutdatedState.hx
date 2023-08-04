6ef389a160de3681a079e30fc76161e6a543c223
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-08-04 13:08:48 +0300

    update source/states/OutdatedState.hx

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -23,6 +23,10 @@ class OutdatedState extends MusicBeatState
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
+
+    #if mobile
+    addVirtualPad(NONE, A_B);
+    #end
 	}
 
 	override function update(elapsed:Float)
