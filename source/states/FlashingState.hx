411de8294496d70cf016f05d6488e3529598eed9
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-08-04 13:06:59 +0300

    update source/states/FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -21,13 +21,17 @@ class FlashingState extends MusicBeatState
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Hey, watch out!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
 			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
+
+    #if mobile
+    addVirtualPad(NONE, A_B);
+    #end
 	}
 
 	override function update(elapsed:Float)
