250c914cdad569e80870266a667e0f1eb7794139
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-08-08 09:10:06 +0300

    update source/substates/GameplayChangersSubstate.hx

--- source/substates/GameplayChangersSubstate.hx
+++ source/substates/GameplayChangersSubstate.hx
@@ -135,6 +135,10 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+    #if mobile
+    addVirtualPad(FULL, A_B_C);
+    #end
 	}
 
 	var nextAccept:Int = 5;
@@ -265,7 +269,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if mobile || MusicBeatState._virtualpad.buttonC.justPressed #end)
 			{
 				for (i in 0...optionsArray.length)
 				{
