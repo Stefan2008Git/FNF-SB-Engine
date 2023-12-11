a36282134f129c64d0f4e11c9d804c18dd624cfc
Stefan2008_Official <126715034+Stefan2008Git@users.noreply.github.com>
2023-12-11 06:29:39 +0100

    SCall => TeaCall again

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -201,7 +201,7 @@ class HScript extends SScript
 		#end
 	}
 
-	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):SCall
+	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):TeaCall
 	{
 		if (funcToRun == null) return null;
 
@@ -227,7 +227,7 @@ class HScript extends SScript
 		return callValue;
 	}
 
-	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):SCall
+	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):TeaCall
 	{
 		if (funcToRun == null)
 			return null;
@@ -239,7 +239,7 @@ class HScript extends SScript
 	{
 		#if LUA_ALLOWED
 		funk.addLocalCallback("runHaxeCode", function(codeToRun:String, ?varsToBring:Any = null, ?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):Dynamic {
-			var retVal:SCall = null;
+			var retVal:TeaCall = null;
 			#if (SScript >= "3.0.0")
 			initHaxeModule(funk);
 			funk.hscript.toString(codeToRun);
