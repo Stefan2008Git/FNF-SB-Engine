c4ecc3b0c92ad9f9a0b3186ef4f51b53648ceec6
Stefan2008_Official <126715034+Stefan2008Git@users.noreply.github.com>
2023-12-11 09:55:02 +0100

    Trying to fix HScript 3 errors

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -5,12 +5,7 @@ import objects.Character;
 import psychlua.FunkinLua;
 import psychlua.CustomSubstate;
 
-#if sys
-import sys.FileSystem;
-import sys.io.File;
-#end
-
-#if (HSCRIPT_ALLOWED && SScript >= "3.0.0")
+#if HSCRIPT_ALLOWED
 import tea.SScript;
 class HScript extends SScript
 {
@@ -18,56 +13,41 @@ class HScript extends SScript
 	
 	public static function initHaxeModule(parent:FunkinLua)
 	{
-		#if (SScript >= "3.0.0")
 		if(parent.hscript == null)
 		{
 			trace('initializing haxe interp for: ${parent.scriptName}');
 			parent.hscript = new HScript(parent);
 		}
-		#end
 	}
 
-	public static function initHaxeModuleCode(parent:FunkinLua, code:String)
+	public static function initHaxeModuleCode(parent:FunkinLua, code:String, ?varsToBring:Any = null)
 	{
-		#if (SScript >= "3.0.0")
-		var hs:HScript = parent.hscript;
+		var hs:HScript = try parent.hscript catch (e) null;
 		if(hs == null)
 		{
 			trace('initializing haxe interp for: ${parent.scriptName}');
-			parent.hscript = new HScript(parent, code);
+			parent.hscript = new HScript(parent, code, varsToBring);
 		}
 		else
 		{
-			hs.toString(code);
+			hs.doString(code);
 			@:privateAccess
-			if(hs.parsingExceptions != null && hs.parsingExceptions.length > 0)
+			if(hs.parsingException != null)
 			{
-				@:privateAccess
-				for (e in hs.parsingExceptions)
-					if(e != null)
-						PlayState.instance.addTextToDebug('ERROR ON LOADING (${hs.origin}): ${e.message.substr(0, e.message.indexOf('\n'))}', FlxColor.RED);
+				PlayState.instance.addTextToDebug('ERROR ON LOADING (${hs.origin}): ${hs.parsingException.message}', FlxColor.RED);
 			}
 		}
-		#end
 	}
 
 	public var origin:String;
-	override public function new(?parent:FunkinLua, ?file:String)
+	override public function new(?parent:FunkinLua, ?file:String, ?varsToBring:Any = null)
 	{
-		var usesClasses = false;
 		if (file == null)
 			file = '';
-	
-		#if sys
-		else if (FileSystem.exists(file)) {
-			var fileWithoutComments = ~/(\/[*](?:[^*]|[\r\n]|([*]+([^*\/]|[\r\n])))*[*]+\/|\/\/.*)/gm.replace(File.getContent(file), '');
-			usesClasses = ~/class\s.*\s*{/.match(fileWithoutComments);
-		}
-		#end
 
-		super(null, false, false);
-		classSupport = usesClasses;
-		doFile(file);
+		this.varsToBring = varsToBring;
+	
+		super(file, false, false);
 		parentLua = parent;
 		if (parent != null)
 			origin = parent.scriptName;
@@ -77,36 +57,38 @@ class HScript extends SScript
 		execute();
 	}
 
+	var varsToBring:Any = null;
 	override function preset()
 	{
-		#if (SScript >= "3.0.0")
 		super.preset();
 
 		// Some very commonly used classes
 		set('FlxG', flixel.FlxG);
+		set('FlxMath', flixel.math.FlxMath);
 		set('FlxSprite', flixel.FlxSprite);
 		set('FlxCamera', flixel.FlxCamera);
 		set('FlxTimer', flixel.util.FlxTimer);
 		set('FlxTween', flixel.tweens.FlxTween);
 		set('FlxEase', flixel.tweens.FlxEase);
-		set('FlxColor', CustomFlxColor.instance);
+		set('FlxColor', CustomFlxColor);
+		set('Countdown', backend.BaseStage.Countdown);
 		set('PlayState', PlayState);
 		set('Paths', Paths);
 		set('Conductor', Conductor);
 		set('ClientPrefs', ClientPrefs);
+		#if ACHIEVEMENTS_ALLOWED
+		set('Achievements', Achievements);
+		#end
 		set('Character', Character);
 		set('Alphabet', Alphabet);
 		set('Note', objects.Note);
 		set('CustomSubstate', CustomSubstate);
-		set('Countdown', backend.BaseStage.Countdown);
 		#if (!flash && sys)
 		set('FlxRuntimeShader', flixel.addons.display.FlxRuntimeShader);
 		#end
 		set('ShaderFilter', openfl.filters.ShaderFilter);
 		set('StringTools', StringTools);
-		#if mobile
-		set('SUtil', SUtil);
-		#end
+	
 
 		// Functions & Variables
 		set('setVar', function(name:String, value:Dynamic)
@@ -161,10 +143,7 @@ class HScript extends SScript
 				if(libPackage.length > 0)
 					str = libPackage + '.';
 
-				var c:Dynamic = Type.resolveClass(str + libName);
-				if (c == null)
-					c = Type.resolveEnum(str + libName);
-				set(libName, c);
+				set(libName, Type.resolveClass(str + libName));
 			}
 			catch (e:Dynamic) {
 				var msg:String = e.message.substr(0, e.message.indexOf('\n'));
@@ -180,6 +159,9 @@ class HScript extends SScript
 		set('parentLua', parentLua);
 		set('this', this);
 		set('game', PlayState.instance);
+		/*
+		if (PlayState.instance != null)
+			setSpecialObject(PlayState.instance, false, PlayState.instance.instancesExclude);*/
 		set('buildTarget', FunkinLua.getBuildTarget());
 		set('customSubstate', CustomSubstate.instance);
 		set('customSubstateName', CustomSubstate.name);
@@ -196,9 +178,18 @@ class HScript extends SScript
 		set('addBehindBF', function(obj:FlxBasic) PlayState.instance.addBehindBF(obj));
 		set('insert', function(pos:Int, obj:FlxBasic) PlayState.instance.insert(pos, obj));
 		set('remove', function(obj:FlxBasic, splice:Bool = false) PlayState.instance.remove(obj, splice));
-		
-		set('Math', Math);
-		#end
+
+		if(varsToBring != null)
+		{
+			for (key in Reflect.fields(varsToBring))
+			{
+				key = key.trim();
+				var value = Reflect.field(varsToBring, key);
+				//trace('Key $key: $value');
+				set(key, Reflect.field(varsToBring, key));
+			}
+			varsToBring = null;
+		}
 	}
 
 	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):TeaCall
@@ -239,28 +230,23 @@ class HScript extends SScript
 	{
 		#if LUA_ALLOWED
 		funk.addLocalCallback("runHaxeCode", function(codeToRun:String, ?varsToBring:Any = null, ?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):Dynamic {
-			var retVal:TeaCall = null;
-			#if (SScript >= "3.0.0")
-			initHaxeModule(funk);
-			funk.hscript.toString(codeToRun);
-			if(varsToBring != null)
-			{
-				for (key in Reflect.fields(varsToBring))
-				{
-					//trace('Key $key: ' + Reflect.field(varsToBring, key));
-					funk.hscript.set(key, Reflect.field(varsToBring, key));
-				}
-			}
-			retVal = funk.hscript.executeCode(funcToRun, funcArgs);
+			#if SScript
+			initHaxeModuleCode(funk, codeToRun, varsToBring);
+			var retVal:TeaCall = funk.hscript.executeCode(funcToRun, funcArgs);
 			if (retVal != null)
 			{
 				if(retVal.succeeded)
 					return (retVal.returnValue == null || LuaUtils.isOfTypes(retVal.returnValue, [Bool, Int, Float, String, Array])) ? retVal.returnValue : null;
 
 				var e = retVal.exceptions[0];
+				var calledFunc:String = if(funk.hscript.origin == funk.lastCalledFunction) funcToRun else funk.lastCalledFunction;
 				if (e != null)
-					FunkinLua.luaTrace(funk.hscript.origin + ":" + funk.lastCalledFunction + " - " + e, false, false, FlxColor.RED);
+					FunkinLua.luaTrace(funk.hscript.origin + ":" + calledFunc + " - " + e, false, false, FlxColor.RED);
 				return null;
+			}
+			else if (funk.hscript.returnValue != null)
+			{
+				return funk.hscript.returnValue;
 			}
 			#else
 			FunkinLua.luaTrace("runHaxeCode: HScript isn't supported on this platform!", false, false, FlxColor.RED);
@@ -269,7 +255,7 @@ class HScript extends SScript
 		});
 		
 		funk.addLocalCallback("runHaxeFunction", function(funcToRun:String, ?funcArgs:Array<Dynamic> = null) {
-			#if (SScript >= "3.0.0")
+			#if SScript
 			var callValue = funk.hscript.executeFunction(funcToRun, funcArgs);
 			if (!callValue.succeeded)
 			{
@@ -296,12 +282,12 @@ class HScript extends SScript
 			if (c == null)
 				c = Type.resolveEnum(str + libName);
 
-			#if (SScript >= "3.0.3")
+			#if SScript
 			if (c != null)
 				SScript.globalVariables[libName] = c;
 			#end
 
-			#if (SScript >= "3.0.0")
+			#if SScript
 			if (funk.hscript != null)
 			{
 				try {
@@ -319,7 +305,6 @@ class HScript extends SScript
 		#end
 	}
 
-	#if (SScript >= "3.0.3")
 	override public function destroy()
 	{
 		origin = null;
@@ -327,163 +312,57 @@ class HScript extends SScript
 
 		super.destroy();
 	}
-	#else
-	public function destroy()
-	{
-		active = false;
-	}
-	#end
 }
-#end
 
-@:publicFields
 class CustomFlxColor
 {
-	static var instance:CustomFlxColor = new CustomFlxColor();
-	function new() {}
+	public static var TRANSPARENT(default, null):Int = FlxColor.TRANSPARENT;
+	public static var BLACK(default, null):Int = FlxColor.BLACK;
+	public static var WHITE(default, null):Int = FlxColor.WHITE;
+	public static var GRAY(default, null):Int = FlxColor.GRAY;
 
-	var TRANSPARENT(default, null):Int = FlxColor.TRANSPARENT;
-	var WHITE(default, null):Int = FlxColor.WHITE;
-	var GRAY(default, null):Int = FlxColor.GRAY;
-	var BLACK(default, null):Int = FlxColor.BLACK;
+	public static var GREEN(default, null):Int = FlxColor.GREEN;
+	public static var LIME(default, null):Int = FlxColor.LIME;
+	public static var YELLOW(default, null):Int = FlxColor.YELLOW;
+	public static var ORANGE(default, null):Int = FlxColor.ORANGE;
+	public static var RED(default, null):Int = FlxColor.RED;
+	public static var PURPLE(default, null):Int = FlxColor.PURPLE;
+	public static var BLUE(default, null):Int = FlxColor.BLUE;
+	public static var BROWN(default, null):Int = FlxColor.BROWN;
+	public static var PINK(default, null):Int = FlxColor.PINK;
+	public static var MAGENTA(default, null):Int = FlxColor.MAGENTA;
+	public static var CYAN(default, null):Int = FlxColor.CYAN;
 
-	var GREEN(default, null):Int = FlxColor.GREEN;
-	var LIME(default, null):Int = FlxColor.LIME;
-	var YELLOW(default, null):Int = FlxColor.YELLOW;
-	var ORANGE(default, null):Int = FlxColor.ORANGE;
-	var RED(default, null):Int = FlxColor.RED;
-	var PURPLE(default, null):Int = FlxColor.PURPLE;
-	var BLUE(default, null):Int = FlxColor.BLUE;
-	var BROWN(default, null):Int = FlxColor.BROWN;
-	var PINK(default, null):Int = FlxColor.PINK;
-	var MAGENTA(default, null):Int = FlxColor.MAGENTA;
-	var CYAN(default, null):Int = FlxColor.CYAN;
+	public static function fromInt(Value:Int):Int 
+	{
+		return cast FlxColor.fromInt(Value);
+	}
 
-	function fromRGB(Red:Int, Green:Int, Blue:Int, Alpha:Int = 255):Int
+	public static function fromRGB(Red:Int, Green:Int, Blue:Int, Alpha:Int = 255):Int
 	{
 		return cast FlxColor.fromRGB(Red, Green, Blue, Alpha);
 	}
-	function getRGB(color:Int):Array<Int>
-	{
-		var flxcolor:FlxColor = FlxColor.fromInt(color);
-		return [flxcolor.red, flxcolor.green, flxcolor.blue, flxcolor.alpha];
-	}
-	function fromRGBFloat(Red:Float, Green:Float, Blue:Float, Alpha:Float = 1):Int
+	public static function fromRGBFloat(Red:Float, Green:Float, Blue:Float, Alpha:Float = 1):Int
 	{	
 		return cast FlxColor.fromRGBFloat(Red, Green, Blue, Alpha);
 	}
-	function getRGBFloat(color:Int):Array<Float>
-	{
-		var flxcolor:FlxColor = FlxColor.fromInt(color);
-		return [flxcolor.redFloat, flxcolor.greenFloat, flxcolor.blueFloat, flxcolor.alphaFloat];
-	}
-	function fromCMYK(Cyan:Float, Magenta:Float, Yellow:Float, Black:Float, Alpha:Float = 1):Int
+
+	public static inline function fromCMYK(Cyan:Float, Magenta:Float, Yellow:Float, Black:Float, Alpha:Float = 1):Int
 	{
 		return cast FlxColor.fromCMYK(Cyan, Magenta, Yellow, Black, Alpha);
 	}
-	function getCMYK(color:Int):Array<Float>
-	{
-		var flxcolor:FlxColor = FlxColor.fromInt(color);
-		return [flxcolor.cyan, flxcolor.magenta, flxcolor.yellow, flxcolor.black, flxcolor.alphaFloat];
-	}
-	function fromHSB(Hue:Float, Sat:Float, Brt:Float, Alpha:Float = 1):Int
+
+	public static function fromHSB(Hue:Float, Sat:Float, Brt:Float, Alpha:Float = 1):Int
 	{	
 		return cast FlxColor.fromHSB(Hue, Sat, Brt, Alpha);
 	}
-	function getHSB(color:Int):Array<Float>
-	{
-		var flxcolor:FlxColor = FlxColor.fromInt(color);
-		return [flxcolor.hue, flxcolor.saturation, flxcolor.brightness, flxcolor.alphaFloat];
-	}
-	function fromHSL(Hue:Float, Sat:Float, Light:Float, Alpha:Float = 1):Int
+	public static function fromHSL(Hue:Float, Sat:Float, Light:Float, Alpha:Float = 1):Int
 	{	
 		return cast FlxColor.fromHSL(Hue, Sat, Light, Alpha);
 	}
-	function getHSL(color:Int):Array<Float>
-	{
-		var flxcolor:FlxColor = FlxColor.fromInt(color);
-		return [flxcolor.hue, flxcolor.saturation, flxcolor.lightness, flxcolor.alphaFloat];
-	}
-	function fromString(str:String):Int
+	public static function fromString(str:String):Int
 	{
 		return cast FlxColor.fromString(str);
 	}
-	function getHSBColorWheel(Alpha:Int = 255):Array<Int>
-	{
-		return cast FlxColor.getHSBColorWheel(Alpha);
-	}
-	function interpolate(Color1:Int, Color2:Int, Factor:Float = 0.5):Int
-	{
-		return cast FlxColor.interpolate(Color1, Color2, Factor);
-	}
-	function gradient(Color1:Int, Color2:Int, Steps:Int, ?Ease:Float->Float):Array<Int>
-	{
-		return cast FlxColor.gradient(Color1, Color2, Steps, Ease);
-	}
-	function multiply(lhs:Int, rhs:Int):Int
-	{
-		return cast FlxColor.multiply(lhs, rhs);
-	}
-	function add(lhs:Int, rhs:Int):Int
-	{
-		return cast FlxColor.add(lhs, rhs);
-	}
-	function subtract(lhs:Int, rhs:Int):Int
-	{
-		return cast FlxColor.subtract(lhs, rhs);
-	}
-	function getComplementHarmony(color:Int):Int
-	{
-		return cast FlxColor.fromInt(color).getComplementHarmony();
-	}
-	function getAnalogousHarmony(color:Int, Threshold:Int = 30):CustomHarmony
-	{
-		return cast FlxColor.fromInt(color).getAnalogousHarmony(Threshold);
-	}
-	function getSplitComplementHarmony(color:Int, Threshold:Int = 30):CustomHarmony
-	{
-		return cast FlxColor.fromInt(color).getSplitComplementHarmony(Threshold);
-	}
-	function getTriadicHarmony(color:Int):CustomTriadicHarmony
-	{
-		return cast FlxColor.fromInt(color).getTriadicHarmony();
-	}
-	function to24Bit(color:Int):Int
-	{
-		return color & 0xffffff;
-	}
-	function toHexString(color:Int, Alpha:Bool = true, Prefix:Bool = true):String
-	{
-		return cast FlxColor.fromInt(color).toHexString(Alpha, Prefix);
-	}
-	function toWebString(color:Int):String
-	{
-		return cast FlxColor.fromInt(color).toWebString();
-	}
-	function getColorInfo(color:Int):String
-	{
-		return cast FlxColor.fromInt(color).getColorInfo();
-	}
-	function getDarkened(color:Int, Factor:Float = 0.2):Int
-	{
-		return cast FlxColor.fromInt(color).getDarkened(Factor);
-	}
-	function getLightened(color:Int, Factor:Float = 0.2):Int
-	{
-		return cast FlxColor.fromInt(color).getLightened(Factor);
-	}
-	function getInverted(color:Int):Int
-	{
-		return cast FlxColor.fromInt(color).getInverted();
-	}
 }
-typedef CustomHarmony = {
-	original:Int,
-	warmer:Int,
-	colder:Int
-}
-typedef CustomTriadicHarmony = {
-	color1:Int,
-	color2:Int,
-	color3:Int
-}
+#end
