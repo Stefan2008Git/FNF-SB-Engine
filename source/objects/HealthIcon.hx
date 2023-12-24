2864213b0810ee21914700b6b043b0f9ba63c911
Stefan Menićanin <126715034+Stefan2008Git@users.noreply.github.com>
2023-12-24 18:56:27 +0100

    Some brand new additons shit - Added an NPS - Trying to fix an Winning icon problem

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -4,9 +4,8 @@ class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
 	private var isOldIcon:Bool = false;
-	public var isPlayer:Bool = false;
+	private var isPlayer:Bool = false;
 	private var char:String = '';
-	public var auto:Bool = true;
 
 	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
 	{
@@ -22,26 +21,26 @@ class HealthIcon extends FlxSprite
 		super.update(elapsed);
 
 		if (sprTracker != null)
-			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
 	}
 
-	private var iconOffsets:Array<Float> = [0, 0, 0];
+	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String, ?allowGPU:Bool = true) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			var file:Dynamic = Paths.image(name);
-
-			loadGraphic(file); //Load stupidly first for getting the file size
+			
+			var graphic = Paths.image(name, allowGPU);
+			loadGraphic(graphic); //Load stupidly first for getting the file size
 			var width2 = width;
 			if (width == 450) {
-				loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr // winning icons go br
+				loadGraphic(graphic, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr // winning icons go br
 				iconOffsets[0] = (width - 150) / 3;
 				iconOffsets[1] = (width - 150) / 3;
 				iconOffsets[2] = (width - 150) / 3;
 			} else {
-				loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr // winning icons go br
+				loadGraphic(graphic, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr // winning icons go br
 				iconOffsets[0] = (width - 150) / 2;
 				iconOffsets[1] = (width - 150) / 2;
 			}
@@ -55,10 +54,10 @@ class HealthIcon extends FlxSprite
 			animation.play(char);
 			this.char = char;
 
-			antialiasing = ClientPrefs.data.antialiasing;
-			if(char.endsWith('-pixel')) {
+			if(char.endsWith('-pixel'))
 				antialiasing = false;
-			}
+			else
+				antialiasing = ClientPrefs.data.antialiasing;
 		}
 	}
 
