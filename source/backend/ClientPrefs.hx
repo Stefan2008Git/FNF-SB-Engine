6a3391961290de1683401877f43fb21a1fb96cc0
Stefan2008_Official <stefanbeta2008@gmail.com>
2024-02-25 05:47:14 +0100

    Nevermind...

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -3,8 +3,6 @@ package backend;
 import flixel.util.FlxSave;
 import flixel.input.keyboard.FlxKey;
 import flixel.input.gamepad.FlxGamepadInputID;
-
-import objects.TraceText;
 import states.TitleState;
 
 // Add a variable here and it will get automatically saved
