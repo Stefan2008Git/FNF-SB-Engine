cd62037dc65cf40f152d06482a26d5028072ed4a
Stefan2008 <126715034+Stefan2008Git@users.noreply.github.com>
2024-05-01 06:38:08 +0200

    You are definitely kidding me i see huh?

--- source/android/backend/Touch.hx
+++ source/android/backend/Touch.hx
@@ -2,27 +2,18 @@ package android.backend;
 
 class Touch
 {
-    **
-     * Returns FlxTouch, shortcut to `FlxG.touches.getFirst()`.
-     * @return FlxTouch
-     */
     public static function touch():FlxTouch {
         return (FlxG.touches.getFirst());
     }
 
-    /**
-     * Touch checker for `spr`, then runs `onTouch` function
-     * @param spr       Sprite that will be checked
-     * @param onTouch   Function that will be runned if Touch = true
-     */
     public static function touchJustPressed(spr:FlxSprite, onTouch:Void -> Void) {
         if (Touch.touch()!=null){
             var sprPos = spr.getScreenPosition(FlxG.camera);
-            var touchX = Android.touch().screenX;
-            var touchY = Android.touch().screenY;
+            var touchX = Touch.touch().screenX;
+            var touchY = Touch.touch().screenY;
             var overlap:Bool = (touchX >= sprPos.x && touchX <= sprPos.x + spr.frameWidth
             && touchY >= sprPos.y && touchY <= sprPos.y + spr.frameHeight);
-            if (overlap && Android.touch().justPressed){
+            if (overlap && Touch.touch().justPressed){
                 onTouch();
             }
         }
