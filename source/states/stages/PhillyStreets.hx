d171fb0a6ee6a927c39736eaeffe67f8c83c63b4
Stefan2008Git <stefanbeta2008@gmail.com>
2024-05-05 21:58:08 +0200

    Weekend 1 port is done like 50% - Chart for all 3 songs are fully finished, but for Hard difficulty - Fixed a philly stage  positions

--- source/states/stages/PhillyStreets.hx
+++ source/states/stages/PhillyStreets.hx
@@ -4,6 +4,7 @@ import objects.BGSprite;
 
 class PhillyStreets extends BaseStage
 {
+    var phillySkyBox:FlxSprite;
     var skyAdditive:FlxSprite;
     var phillyForegroundCity:FlxSprite;
     var phillyConstruction:FlxSprite;
@@ -11,34 +12,42 @@ class PhillyStreets extends BaseStage
     var phillyHighwayLightsLightmap:FlxSprite;
     var phillyHighway:FlxSprite;
     var phillySmog:FlxSprite;
+    var phillyTraffic:FlxSprite;
     var phillyTrafficLightmap:FlxSprite;
     var phillyForeground:FlxSprite;
     var spraycanPile:FlxSprite;
 
     override function create() 
     {
-        skyAdditive = new BGSprite('phillyStreets/phillySkyline', -545, -273);
+        phillySkyBox = new BGSprite('phillyStreets/phillySkybox', -700, -200);
+        phillySkyBox.scrollFactor.set(0.3, 0.3);
+        phillySkyBox.alpha = 1;
+        phillySkyBox.scale.x = 1;
+        phillySkyBox.scale.y = 5;
+        add(phillySkyBox);
+
+        skyAdditive = new BGSprite('phillyStreets/phillySkyline', -700, -300);
         skyAdditive.scrollFactor.set(0.2, 0.2);
         skyAdditive.alpha = 1;
         skyAdditive.scale.x = 1;
         skyAdditive.scale.y = 1;
         add(skyAdditive);
 
-        phillyForegroundCity = new BGSprite('phillyStreets/phillyForegroundCity', 625, 94);
+        phillyForegroundCity = new BGSprite('phillyStreets/phillyForegroundCity', 350, -20);
         phillyForegroundCity.scrollFactor.set(0.3, 0.3);
         phillyForegroundCity.alpha = 1;
         phillyForegroundCity.scale.x = 1;
         phillyForegroundCity.scale.y = 1;
         add(phillyForegroundCity);
 
-        phillyConstruction = new BGSprite('phillyStreets/phillyConstruction', 1800, 364);
+        phillyConstruction = new BGSprite('phillyStreets/phillyConstruction', 1000, -150);
         phillyConstruction.scrollFactor.set(0.7, 1);
         phillyConstruction.alpha = 1;
         phillyConstruction.scale.x = 1;
         phillyConstruction.scale.y = 1;
         add(phillyConstruction);
 
-        phillyHighwayLights = new BGSprite('phillyStreets/phillyHighwayLights', 284, 305);
+        phillyHighwayLights = new BGSprite('phillyStreets/phillyHighwayLights', -1050, -205);
         phillyHighwayLights.scrollFactor.set(1, 1);
         phillyHighwayLights.alpha = 1;
         phillyHighwayLights.scale.x = 1;
@@ -52,35 +61,42 @@ class PhillyStreets extends BaseStage
         phillyHighwayLightsLightmap.scale.y = 1;
         add(phillyHighwayLightsLightmap);
 
-        phillyHighway = new BGSprite('phillyStreets/phillyHighway', 139, 209);
+        phillyHighway = new BGSprite('phillyStreets/phillyHighway', -1130, -250);
         phillyHighway.scrollFactor.set(1, 1);
         phillyHighway.alpha = 1;
         phillyHighway.scale.x = 1;
         phillyHighway.scale.y = 1;
         add(phillyHighway);
 
-        phillySmog = new BGSprite('phillyStreets/phillySmog', -6, 245);
+        phillySmog = new BGSprite('phillyStreets/phillySmog', -1000, -200);
         phillySmog.scrollFactor.set(1, 1);
         phillySmog.alpha = 1;
         phillySmog.scale.x = 1;
         phillySmog.scale.y = 1;
         add(phillySmog);
 
-        phillyTrafficLightmap = new BGSprite('phillyStreets/phillyTraffic_lightmap', 1840, 608);
+        phillyTraffic = new BGSprite('phillyStreets/phillyTraffic', 1840, 608);
+        phillyTraffic.scrollFactor.set(0.9, 1);
+        phillyTraffic.alpha = 1;
+        phillyTraffic.scale.x = 1;
+        phillyTraffic.scale.y = 1;
+        add(phillyTraffic);
+
+        phillyTrafficLightmap = new BGSprite('phillyStreets/phillyTraffic_lightmap', -1100, -100);
         phillyTrafficLightmap.scrollFactor.set(0.9, 1);
         phillyTrafficLightmap.alpha = 1;
         phillyTrafficLightmap.scale.x = 1;
         phillyTrafficLightmap.scale.y = 1;
         add(phillyTrafficLightmap);
 
-        phillyForeground = new BGSprite('phillyStreets/phillyForeground', 88, 317);
+        phillyForeground = new BGSprite('phillyStreets/phillyForeground', -1430, -250);
         phillyForeground.scrollFactor.set(1, 1);
         phillyForeground.alpha = 1;
         phillyForeground.scale.x = 1;
         phillyForeground.scale.y = 1;
         add(phillyForeground);
 
-        spraycanPile = new BGSprite('SpraycanPile', 92, 1045);
+        spraycanPile = new BGSprite('SpraycanPile', -320, 630);
         spraycanPile.scrollFactor.set(1, 1);
         spraycanPile.alpha = 1;
         spraycanPile.scale.x = 1;
