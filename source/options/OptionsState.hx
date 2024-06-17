98aafc332b0fb90d4891ba840f097545070dbadb
Stefan <126715034+Stefan2008Git@users.noreply.github.com>
2024-06-18 01:26:08 +0200

    I think i prepared the plan B

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -6,7 +6,7 @@ import flixel.addons.transition.FlxTransitionableState;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', #if desktop 'Controls', #end 'Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay', #if android 'Android Options' #end];
+	var options:Array<String> = ['Note Colors', #if desktop 'Controls', #end 'Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay', #if android 'Android Options', 'Android Controls' #end];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var currentlySelected:Int = 0;
 	public static var onPlayState:Bool = false;
@@ -48,12 +48,15 @@ class OptionsState extends MusicBeatState
 				removeVirtualPad();
 				openSubState(new options.android.AndroidOptionsSubState());
 				#end
+			case 'Android Controls':
+				FlxTransitionableState.skipNextTransIn = true;
+			        FlxTransitionableState.skipNextTransOut = true;
+			        FlxG.switchState(() -> new options.android.AndroidControlsMenuState());     
 		}
 	}
 
 	var background:FlxSprite;
 	var checkerboard:FlxBackdrop;
-	var androidControlsStyleTipText:FlxText;
 	var selectorLeft:Alphabet;
 	var selectorRight:Alphabet;
 	var gearIcon:FlxSprite;
@@ -99,7 +102,7 @@ class OptionsState extends MusicBeatState
 		{
 			var optionText:Alphabet = new Alphabet(0, 0, options[i], true);
 			optionText.screenCenter();
-			optionText.y += (85 * (i - (options.length / 2))) + 100;
+			optionText.y += (85 * (i - (options.length / 2))) + #if android 93 #else 100 #end;
 			optionText.x = 135;
 			grpOptions.add(optionText);
 		}
@@ -109,13 +112,13 @@ class OptionsState extends MusicBeatState
 		selectorRight = new Alphabet(0, 0, '<', true);
 		add(selectorRight);
 
-		gearIcon = new FlxSprite(110, 26).loadGraphic(Paths.image('engineStuff/options/main/gearIcon'));
+		gearIcon = new FlxSprite(110, #if android 22 #else 26 #end).loadGraphic(Paths.image('engineStuff/options/main/gearIcon'));
 		gearIcon.scale.x = 0.7;
 		gearIcon.scale.y = 0.7;
 		gearIcon.antialiasing = ClientPrefs.data.antialiasing;
 		add(gearIcon);
 
-		optionsMenu = new FlxSprite(130, 26).loadGraphic(Paths.image('engineStuff/options/main/optionsMenu'));
+		optionsMenu = new FlxSprite(130, #if android 22 #else 26 #end).loadGraphic(Paths.image('engineStuff/options/main/optionsMenu'));
 		optionsMenu.scale.x = 0.6;
 		optionsMenu.scale.y = 0.6;
 		optionsMenu.antialiasing = ClientPrefs.data.antialiasing;
@@ -134,31 +137,12 @@ class OptionsState extends MusicBeatState
 		add(optionsTipTxtBG);
 		add(optionsTipTxt);
 
-		#if android
-		androidControlsStyleTipText = new FlxText(150, FlxG.height - 24, 0, "Press C to open the Android controls style!", 16);
-		switch (ClientPrefs.data.gameStyle) {
-			case 'SB Engine': 
-				androidControlsStyleTipText.setFormat(Paths.font("bahnschrift.ttf"), 18, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-
-			case 'TGT Engine': 
-				androidControlsStyleTipText.setFormat(Paths.font("calibri.ttf"), 18, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-			case 'Dave and Bambi': 
-				androidControlsStyleTipText.setFormat(Paths.font("comic.ttf"), 18, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-
-			default:
-				androidControlsStyleTipText.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		}
-		androidControlsStyleTipText.borderSize = 1.25;
-		androidControlsStyleTipText.scrollFactor.set();
-		add(androidControlsStyleTipText);
-		#end
-
 		if (ClientPrefs.data.cacheOnGPU) Paths.clearUnusedMemory();
 
 		changeSelection();
 		ClientPrefs.saveSettings();
 
-		#if android addVirtualPad(UP_DOWN, A_B_C); #end
+		#if android addVirtualPad(UP_DOWN, A_B); #end
 
 		super.create();
 	}
@@ -171,16 +155,17 @@ class OptionsState extends MusicBeatState
 	override function update(elapsed:Float) {
 		super.update(elapsed);
 
-		#if android
-		if (MusicBeatState.virtualPad.buttonC.justReleased) // Just in case to see if will open the substate
-			FlxTransitionableState.skipNextTransIn = true;
+		// Archived because of one bug
+		/*#if android
+		if (MusicBeatState.virtualPad.buttonC.justReleased)
+		        FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
-			FlxG.switchState(() -> new options.android.AndroidControlsMenuState());
+			FlxG.switchState(() -> new options.android.AndroidControlsMenuState());	
 
-		/*if (MusicBeatState.virtualPad.buttonY.justReleased) // Archived
+		if (MusicBeatState.virtualPad.buttonY.justReleased)
 			removeVirtualPad();
-			openSubState(new options.android.AndroidOptionsSubState());*/
-		#end
+			openSubState(new options.android.AndroidOptionsSubState());
+		#end*/
 
 		if (controls.UI_UP_P) {
 			changeSelection(-1);
