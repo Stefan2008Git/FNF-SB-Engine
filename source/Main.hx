fb4429891d540341855bdc6e8202e6c13ce9b183
Stefan2008Git <stefanbeta2008@gmail.com>
2024-08-31 19:42:44 +0200

    Updating Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -65,7 +65,7 @@ class Main extends Sprite
 		#end
 		backend.CrashHandler.init();
 
-		#if android AndroidToast.makeText("Welcome to FNF': SB Engine v" + MainMenuState.sbEngineVersion); #end
+		#if android AndroidToast.makeText("Welcome to FNF': SB Engine v" + MainMenuState.sbEngineVersion, 1, 1, 1, 1); #end
 
 		#if windows
 		@:functionCode("
