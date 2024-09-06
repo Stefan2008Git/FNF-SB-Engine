740faa9418de721e21b8c3e41fa7f25a187ab7f3
Stefan2008Git <stefanbeta2008@gmail.com>
2024-09-06 14:26:56 +0200

    Adding the setup script folder for compilers

---
+++ setup/setup-unix.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/linux
+cd ..
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
+haxelib install lime 8.0.2 --quiet
+haxelib install openfl --quiet
+haxelib install flixel --quiet
+haxelib install flixel-addons --quiet
+haxelib install flixel-tools --quiet
+haxelib install hscript-iris 1.0.2 -quiet
+haxelib install tjson --quiet
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7 --quiet
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc e2e60371cc9e705417533a9421feacb91fa25113 --quiet --skip-dependencies
+haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc 70e7f5f3e76d526ac6fb8f0e6665efe7dfda589d --quiet --skip-dependencies
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis d5361037efa3a02c4ab20b5bd14ca11e7d00f519 --quiet --skip-dependencies
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+echo Finished!
+haxelib list
